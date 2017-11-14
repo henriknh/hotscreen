745edd880603d86cbef28889e84c27c7b429e786
Mikael Hedkvist <mikhed-1@student.ltu.se>
2017-11-14 16:35:52 +0100

    Create run.py file to run app

---
+++ server/run.py
@@ -0,0 +1,11 @@
+from server import app
+
+def start():
+	try:
+		app.run(host='0.0.0.0')
+	except Exception:
+		print('Could not start server')
+		raise
+
+if __name__ == '__main__':
+	start()
--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,4 +1,3 @@
 from flask import Flask, request
 app = Flask(__name__)
-
-import server.views
+from server import views
