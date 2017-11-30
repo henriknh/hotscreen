3e003bf2c08daaeb92b6d5660e034bf7db609bb0
Marcus Johansson <johansson.marcus94@gmail.com>
2017-11-30 14:54:31 +0100

    something

--- server/server/static/piConnect.js
+++ server/server/static/piConnect.js
@@ -3,10 +3,6 @@ var ip = "";
 var socket;
 console.log(ip);
 
-
-
-
-
 function setIP(ip ,port) {
     ip = 'ws://' + ip + ':' + port + '/controller';
     console.log(ip);
@@ -22,6 +18,34 @@ function setIP(ip ,port) {
     socket.on('updated', function() {
             console.log('updated and finnished');
     });
+    socket.on('lobbystate', function(lobbystate) {
+    lobbystate = JSON.parse(lobbystate);
+    if(lobbystate == "lobby") {
+        document.getElementById('lobby').style.display='block';
+        document.getElementById('game').style.display='none';
+        document.getElementById('loading').style.display='none';
+        document.getElementById('gameover').style.display='none';
+    } else if (lobbystate == "game") {
+        document.getElementById('lobby').style.display='none';
+        document.getElementById('game').style.display='block';
+        document.getElementById('loading').style.display='none';
+        document.getElementById('gameover').style.display='none';
+    } else if (lobbystate == "loading") {
+        document.getElementById('lobby').style.display='none';
+        document.getElementById('game').style.display='none';
+        document.getElementById('loading').style.display='block';
+        document.getElementById('gameover').style.display='none';
+    } else if (lobbystate == "gameover") {
+        document.getElementById('lobby').style.display='none';
+        document.getElementById('game').style.display='none';
+        document.getElementById('loading').style.display='none';
+        document.getElementById('gameover').style.display='block';
+    }
+
+    gameState = {};
+    lastGameState = {};
+    });
+
 }
 
 var movement = {};
@@ -38,24 +62,40 @@ setInterval(function(){
 }, 1000/10);
 
 function setOrientation(event) {
-    movement.alpha = event.alpha;
-    movement.beta = event.beta;
-    movement.gamma = event.gamma;
+    if(window.DeviceOrientationEvent){
+
+        movement.alpha = event.alpha;
+        movement.beta = event.beta;
+        movement.gamma = event.gamma;
+    }
+    else{
+        movement.alpha = 0;
+        movement.beta = 0;
+        movement.gamma = 0;
+    }
+    
 
 }
 function setMotion(event) {
-    movement.x = event.acceleration.x;
-    movement.y = event.acceleration.y;
-    movement.z = event.acceleration.z;
+    if(window.DeviceOrientationEvent){
+
+        movement.x = event.acceleration.x;
+        movement.y = event.acceleration.y;
+        movement.z = event.acceleration.z;        
+    }
+    else{   
+        movement.x = 0;
+        movement.y = 0;
+        movement.z = 0;    
+    }
+
 }
 function moveUp() {
     console.log('movedup');
-    //movement.up = 1;
 }
 
 function moveDown() {
     console.log('moveddown');
-    //movement.down = 1;
 }
 
 window.addEventListener('devicemotion', setMotion , true);
