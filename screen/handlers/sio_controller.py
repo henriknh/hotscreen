4579bc18d94377170f01a4212821767f084ddc32
Your Name <you@example.com>
2017-11-30 13:40:17 +0100

    Fixed stuff

--- screen/handlers/sio_controller.py
+++ screen/handlers/sio_controller.py
@@ -5,7 +5,8 @@ from game import set_movement
 
 from flask import request
 from flask_socketio import emit, join_room, leave_room
+import json
 
 @socketio.on('movement', namespace='/controller')
 def on_movement(movement):
-    set_movement(request.sid, movement)
+    set_movement(request.sid, json.loads(movement))
